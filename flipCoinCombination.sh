a2da53d8e1d85746c4a77908307056543bfa5d45
priyanka-teke05 <priyankateke1@gmail.com>
2020-08-16 22:29:01 +0530

    ADDED: calculate singlet combination and its percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,8 +3,36 @@
 #display welcome message
 echo "Welcome to Flip Coin Combination"
 
-if [ $((RANDOM%2)) -eq 0 ]; then
-	echo "Head"
-else
-	echo "Tail"
-fi
+#dictionary
+declare -A coinCombination
+
+#variable
+key=""
+
+#Function for flipping the coin
+function flipCoin()
+{
+	local flips=$1
+	for(( i=1;i<=flips;i++ ))
+	do
+		if [ $((RANDOM%2)) -eq 0 ]; then
+			key=H
+		else
+			key=T
+		fi
+		coinCombination[$key]=$((${coinCombination[$key]}+1))
+	done
+	calculateCombinationPercentage
+}
+
+#Calculating percentage of each combinations
+function calculateCombinationPercentage()
+{
+	for keys in ${!coinCombination[@]}
+	do
+		coinCombination[$keys]=$((${coinCombination[$keys]}*100/$flips))
+	done
+}
+
+read -p "How many times you want to flips the coin : " flips
+flipCoin $flips
